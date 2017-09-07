88c6caff5cf4c7d20b2306023a11965348bb6aa1
MidnightDrifter <sean.higgins@digipen.edu>
2017-09-06 18:03:25 -0700

    General details written down, still too fuzzy and ill-defined to really figure out where to go / how to proceed.

--- Project1/Project1/agent.cpp
+++ Project1/Project1/agent.cpp
@@ -1,6 +1,29 @@
 #include <ctime> //for random seed
 #include <cmath> 
+#include <queue>
+#include <stack>
 #include "agent.h"
+
+struct Nodes;
+struct BoardState;
+
+
+#define SEARCH_TYPE 0;  //0 = breadth first search, 1 = depth-first search
+//#define CONTAINER_TYPE;
+
+#if SEARCH_TYPE==0
+#define CONTAINER = std::queue<BoardStates>  //???
+
+#endif
+
+#if SEARCH_TYPE==1
+
+#define CONTAINER = std::stack<BoardStates>
+
+#endif
+
+struct BoardState;
+
 
 Agent::Agent(int random_seed) { 
   //supplying your own seed may help debugging, same seed will cause 
@@ -8,6 +31,33 @@ Agent::Agent(int random_seed) {
   if (random_seed==0) std::srand( static_cast<unsigned>(std::time(0))); // random seed from time
   else                std::srand( random_seed ); // random seed from user
 } 
+
+
+
+
+//Search:
+/*
+
+1.  Pop board state from openList.  (Assuming initial is pushed on, then immiedately popped)
+2.  If goal state, STORE IT SOMEWHERE, return to 1
+3.  Else, determine viable, legal moves
+4.  Push board state after those moves onto openList (be it a stack or queue).  
+5.  LOOP
+
+
+
+*/
+
+//Need to keep track of:  # of steps taken, # of dust bunnies cleaned, pointer / reference to previous board state (action taken?)
+//ALWAYS ALWAYS ALWAYS end on home space
+
+
+
+
+
+
+
+
 
 Action Agent::GetAction(Percept p) {
   //straight-forward beahavior
@@ -18,5 +68,13 @@ Action Agent::GetAction(Percept p) {
   return (std::rand() % 5 < 3) ? 
                FORWARD :
                ((std::rand() % 2) ? LEFT : RIGHT);
+
+
+
+  //Breadth-first search:  queue
+
+  
+
+
 }
 
