35f0d764f0c724cb09d0fe0732ed3aa6dfdc010c
MidnightDrifter <sean.higgins@digipen.edu>
2017-09-20 18:12:50 -0700

    Logic inputted I think, wheeeeee

--- Project1/Project1/agent.cpp
+++ Project1/Project1/agent.cpp
@@ -25,7 +25,7 @@ struct BoardState;
 struct BoardState;
 
 
-Agent::Agent(int random_seed) : dirtSucked(0) { 
+Agent::Agent(int random_seed) : currentX(startingX), currentY(startingY), dir(Direction::NORTH), maze(), openList(1, maze.graph[20][20]) {
   //supplying your own seed may help debugging, same seed will cause 
   //same random number sequence 
   if (random_seed==0) std::srand( static_cast<unsigned>(std::time(0))); // random seed from time
@@ -61,17 +61,183 @@ Agent::Agent(int random_seed) : dirtSucked(0) {
 
 Action Agent::GetAction(Percept p) {
   //straight-forward beahavior
+
+
+
 	if (p.dirt) {
-		dirtSucked++; return SUCK;
+		 return SUCK;
 	}
-	if (p.bump) { return (std::rand() % 5 < 3) ? LEFT : RIGHT; } // P(LEFT) = 3/5
-  if (p.home && dirtSucked>0) { return  SHUTOFF; }
+
+
+	bool isValid[4] = { maze.isValidNode(currentX+1 , currentY),  maze.isValidNode(currentX-1 , currentY),maze.isValidNode(currentX , currentY + 1),maze.isValidNode(currentX ,currentY - 1) };
+	bool isVisited[4] = { (isValid[0]&&maze.getNode(currentX + 1, currentY).isVisited), (isValid[1]&&maze.getNode(currentX - 1,currentY).isVisited),(isValid[2]&& maze.graph[currentX][currentY+1].isVisited),(isValid[3]&& maze.graph[currentX][currentY-1].isVisited) };
+	//North node, south node, east node, west node
+
+
+	//REMEMBER:  NORTH = UP, UP = X INCREASES
+	//			SOUTH = DOWN, DOWN = X DECREASES
+	//			EAST = RIGHT, RIGHT = Y INCREASES
+	//			WEST = LEFT, LEFT = Y DECREASES
+	
+
+	if (p.bump) { //return (std::rand() % 5 < 3) ? LEFT : RIGHT;// P(LEFT) = 3/5
+
+		if (dir == Direction::NORTH && isValid[0] )
+		{
+			maze.graph[currentX + 1][currentY ].isWall = true;
+			maze.graph[currentX + 1][currentY].isVisited = true;
+
+		}
+		else if (dir == Direction::SOUTH&& isValid[1])
+		{
+			maze.graph[currentX - 1][currentY ].isWall = true;
+			maze.graph[currentX - 1][currentY].isVisited = true;
+		}
+		else if (dir == Direction::EAST && isValid[2])
+		{
+			maze.getNode(currentX, currentY + 1).isWall = true;
+			maze.getNode(currentX, currentY + 1).isVisited = true;
+		}
+		else if (dir == Direction::WEST && isValid[3])
+		{
+			maze.graph[currentX][currentY - 1].isWall = true;
+			maze.graph[currentX][currentY - 1].isVisited = true;
+		}
+
+
+
+	}
+
   //default rule
+  /*
   return (std::rand() % 5 < 3) ? 
                FORWARD :
                ((std::rand() % 2) ? LEFT : RIGHT);
 
+			   */
 
+  if (isValid[0] && !isVisited[0])  //if valid node and north ISN'T VISITED, try and visit north
+  {
+	  if (dir == Direction::NORTH)
+	  {
+		    //We must be facing north--onwards!
+			  return FORWARD;
+		  
+	  }
+
+	  if (dir == Direction::EAST)
+	  {
+		  //Turn to go north--turn left, then forward
+		  dir = Direction::NORTH;
+		  return LEFT;
+	  }
+	  else if (dir == Direction::WEST)
+	  {
+		  //Turn to go north--turn right, then forward
+		  dir = Direction::NORTH;
+		  return RIGHT;
+	  }
+
+	  else   //Must be facing south
+	  {//Turn to go north--left, left, forward
+		  dir = Direction::EAST;
+		  return LEFT;
+	  }
+
+	  
+
+
+
+  }
+
+  else if(isValid[1] && !isVisited[1])  //"" & south isn't visited, try and visit south
+  {
+	  if (dir == Direction::SOUTH)
+	  {
+		  return FORWARD;
+	  }
+
+	  else if (dir == Direction::EAST)
+	  {
+		  //Turn left to face South
+		  dir = Direction::SOUTH;
+		  return RIGHT;
+	  }
+
+	  else if (dir == Direction::WEST)
+	  {
+		  //Turn right to face South
+		  dir = Direction::SOUTH;
+		  return LEFT;
+	  }
+
+	  else
+	  {
+		  //Turn left, left to face south
+		  dir = Direction::WEST;
+		  return LEFT;
+	  }
+	  
+  }
+
+  else if (isValid[2] && !isVisited[2]) //  East isn't visited
+  {
+	  if (dir == Direction::EAST)
+	  {
+		  return FORWARD;
+	  }
+
+	  else if (dir == Direction::NORTH)
+	  {
+		  dir = Direction::EAST;
+		  return RIGHT;
+	  }
+
+	  else if (dir == Direction::SOUTH)
+	  {
+		  dir = Direction::EAST;
+		  return LEFT;
+	  }
+
+	  else
+	  {
+		  dir = Direction::NORTH;
+		  return RIGHT;
+
+	  }
+  }
+  else if (isValid[3] && !isVisited[3])  //West isn't visited
+  {
+	  if (dir == Direction::WEST)
+	  {
+		  return FORWARD;
+	  }
+
+	  else if (dir == Direction::NORTH)
+	  {
+		  dir = Direction::WEST;
+		  return LEFT;
+	  }
+
+	  else if (dir == Direction::SOUTH)
+	  {
+		  dir = Direction::WEST;
+		  return RIGHT;
+	  }
+
+	  else
+	  {
+		  dir = Direction::NORTH;
+		  return LEFT;
+	  }
+
+  
+  
+  
+  }
+
+
+  if (p.home) { return  SHUTOFF; }
 
   //Breadth-first search:  queue
 
--- Project1/Project1/agent.h
+++ Project1/Project1/agent.h
@@ -1,7 +1,13 @@
 #ifndef AGENT_H
 #define AGENT_H
 #include "definitions.h"
-
+enum class Direction
+{
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST
+};
 class Node
 {
 public:
@@ -27,11 +33,11 @@ public:
 
 		for(int i=0;i<wallSize;i++)
 		{
-			graph.at(i) = std::vector<Node>();
+			graph.push_back( std::vector<Node>());
 
 			for (int j = 0; j < wallSize; j++)
 			{
-				graph.at(i).at(j) = Node(i, j);
+				graph.at(i).push_back( Node(i, j));
 			}
 		}
 	}
@@ -39,6 +45,7 @@ public:
 	~NodeGraph() {}
 	bool isValidNode(int x, int y) { return (x >= 0 && y >= 0 && x < wallSize && y < wallSize); }
 
+	Node& getNode(int x, int y) { return graph[x][y]; }
 
 	std::vector<std::vector<Node>> graph;
 	const int wallSize = 40;
@@ -51,13 +58,22 @@ public:
 
 class Agent {
   public:
-    Agent(int random_seed);
+	  Agent(int random_seed);
     Action GetAction(Percept p);
 
 	
 	//int dirtSucked;
 	const int startingX = 20;
 	const int startingY = 20;
+	int currentX, currentY;
+	//Assume heading defaults to NORTH
+	//REMEMBER:  NORTH = UP, UP = X INCREASES
+	//			SOUTH = DOWN, DOWN = X DECREASES
+	//			EAST = RIGHT, RIGHT = Y INCREASES
+	//			WEST = LEFT, LEFT = Y DECREASES
+	Direction dir;// = Direction::NORTH;
+	NodeGraph maze;
+	std::vector<Node> openList;
 };
 
 #endif
